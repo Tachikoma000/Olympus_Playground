8dae2cc24b60ac03b5ee64a6f0d35002f531a4b4
Tachikoma000 <0thtachikoma@gmail.com>
2021-09-23 16:49:28 -0500

    home page content

--- Playgrounds_Application/Simulators/Pages/home.py
+++ Playgrounds_Application/Simulators/Pages/home.py
@@ -30,4 +30,8 @@ def app():
         Special thanks to the Data and Metrics team! 
         ''')
     st.write('--------------')
-    st.image(data_metrics_vision)
+    col1, col2 = st.columns((1,2))
+    with col1:
+        st.image(data_metrics_vision)
+    with col2:
+        st.subheader('Vision')
