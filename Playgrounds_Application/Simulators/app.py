f82be73a10fb29b67f4c9b6f5091e53360ec21a0
Tachikoma000 <0thtachikoma@gmail.com>
2021-09-29 22:16:30 -0500

    Push for coming soon page

--- Playgrounds_Application/Simulators/app.py
+++ Playgrounds_Application/Simulators/app.py
@@ -1,27 +1,75 @@
-"""Main module for the stream lit app"""
+# ==============THE LIBRARIES
+# region Description: Import all required libraries for this simulator
+#from Pages import home, stakingSimulator, stakingSimulator_Learn, bondingSimulator_Learn, bondingSimulator
+import json
 import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit import session_state
+#from SessionState import _get_state
+
+import urllib.request
+import requests
 
 from PIL import Image
-from Pages import home, stakingSimulator, stakingSimulator_Learn, bondingSimulator_Learn, bondingSimulator
 import pathlib
 from pathlib import Path
-# st.set_page_config(layout="wide")
+# endregion
 
-navLogo = Path(__file__).parents[0] / 'Assets/Asset_3.png'
+
+navLogo = Path(__file__).parents[1] / 'Assets/Asset_3.png'
 navLogo = Image.open(navLogo)
-PAGES = {
-    "Home": home,
-    "Staking: Learn": stakingSimulator_Learn,
-    "Staking:  Playground": stakingSimulator,
-    "Bonding: Learn": bondingSimulator_Learn,
-    "Bonding: Playground": bondingSimulator
-}
 
-st.sidebar.image(navLogo)
-st.sidebar.write('------------------')
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("", list(PAGES.keys()))
-page = PAGES[selection]
+#animatedLoading = Path(__file__).parents[1] /'Assets/sphere_dots_intro.json'
 
-st.sidebar.write('------------------')
-page.app()
+
+#state = _get_state()
+
+st.page_config = st.set_page_config(
+    page_title="Olympus Playgrounds", page_icon=':bar_chart:'
+)
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+def load_lottiefile(filepath: str):
+    with open(filepath, 'r') as f:
+        return json.load(f)
+
+lottie_hello = load_lottiefile('12855-dot-cluster-loader.json')
+
+col1, col2, col3 = st.columns((1, 5, 1))
+
+with col1:
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+with col2:
+    st.image(navLogo)
+    st.markdown("<h3 style='text-align: center; color: white; font-famil:fieldwork'>Soon™</h3>", unsafe_allow_html=True)
+st.write('------------------')
+with col3:
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+    st.write(' ')
+
+lottie_waiting = load_lottieurl('https://assets7.lottiefiles.com/packages/lf20_wvwimamz.json')
+st_lottie(lottie_waiting, speed=1, reverse=False, loop=True, renderer='svg', height=200, key=None)
+
+
+#https://assets7.lottiefiles.com/packages/lf20_wvwimamz.json
+#https://assets10.lottiefiles.com/packages/lf20_hhvgzywn.json
+#https://assets3.lottiefiles.com/packages/lf20_75vM6p.json
+#https://assets8.lottiefiles.com/packages/lf20_yygwoab7.json
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
---
+++ Playgrounds_Application/Simulators/backupApp.py
@@ -0,0 +1,38 @@
+"""Main module for the stream lit app"""
+import streamlit as st
+
+from PIL import Image
+from Pages import home, stakingSimulator, stakingSimulator_Learn, bondingSimulator_Learn, bondingSimulator
+import pathlib
+from pathlib import Path
+# st.set_page_config(layout="wide")
+
+navLogo = Path(__file__).parents[0] / 'Assets/Asset_3.png'
+navLogo = Image.open(navLogo)
+PAGES = {
+    "Home": home,
+    "Staking: Learn": stakingSimulator_Learn,
+    "Staking:  Playground": stakingSimulator,
+    "Bonding: Learn": bondingSimulator_Learn,
+    "Bonding: Playground": bondingSimulator
+}
+
+st.sidebar.image(navLogo)
+st.sidebar.write('------------------')
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("", list(PAGES.keys()))
+page = PAGES[selection]
+
+st.sidebar.write('------------------')
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+
+page.app()
