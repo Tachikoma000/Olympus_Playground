8d84854ab0d0d030db60c05c1d929dad77c305de
Tachikoma000 <0thtachikoma@gmail.com>
2021-10-04 14:48:13 -0500

    config file

--- Playgrounds_Application/Simulators/appExperimental.py
+++ Playgrounds_Application/Simulators/appExperimental.py
@@ -23,13 +23,13 @@ page = PAGES[selection]
 
 st.sidebar.write('------------------')
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+#hide_streamlit_style = """
+ #           <style>
+  #          #MainMenu {visibility: hidden;}
+   #         footer {visibility: hidden;}
+   #         </style>
+      #      """
+#st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 
