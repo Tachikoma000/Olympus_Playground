e2dc17d88184dacfbb6cd043119300cb976950f0
Tachikoma000 <0thtachikoma@gmail.com>
2021-10-04 16:22:27 -0500

    menu

--- Playgrounds_Application/Simulators/appExperimental.py
+++ Playgrounds_Application/Simulators/appExperimental.py
@@ -26,8 +26,8 @@ st.sidebar.write('------------------')
 
 hide_streamlit_style = """
             <style>
-            MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
