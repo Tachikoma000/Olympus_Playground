bdd55c46bbb6391b6dcca00aaca3f9a77043246d
Tachikoma000 <0thtachikoma@gmail.com>
2021-10-07 17:46:34 -0500

    updates

--- Playgrounds_Application/Simulators/Experimental/mainDashboard.py
+++ Playgrounds_Application/Simulators/Experimental/mainDashboard.py
@@ -5,6 +5,7 @@
 import math  # Needed for basic math operations\n",
 import pandas as pd  # Needed fpr dataframe creation and operations\n",
 import numpy as np  # Needed for array manipulations\n",
+from millify import millify
 from itertools import islice  # Needed for more complex row and coloumn slicing\n",
 import matplotlib.pyplot as plt  # Needed for quickly ploting results"
 import pathlib  # url management
@@ -74,9 +75,16 @@ def app():
 
     st.title('Playgrounds Ω Explorer')
     st.markdown('''----''')
-
     st.sidebar.info('Some information on what this page is about')
     st.sidebar.info('Instructions')
+
+
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric(label="OHM Price", value ="$ %.2f" % protocolMetrics_df.ohmPrice.iloc[0])
+    col2.metric(label="Market Cap", value = millify(protocolMetrics_df.marketCap.iloc[0], precision = 3))
+    col3.metric(label="Total Supply", value = millify(protocolMetrics_df.totalSupply.iloc[0], precision = 3))
+    col4.metric(label="Rebase Rate", value="%.4f" % protocolMetrics_df.nextEpochRebase.iloc[0])
+    st.markdown('''----''')
 
     cols = ["ohmCirculatingSupply", "sOhmCirculatingSupply", "totalSupply"]
     st.subheader('Select metrics to explore')
@@ -84,10 +92,10 @@ def app():
     selected_metric_df = protocolMetrics_df[selected_metric]
 
     st.subheader('Filter by date')
-    col1,col2 = st.columns((1,1))
-    with col1:
+    col5,col6 = st.columns((1,1))
+    with col5:
         startDate = st.date_input('Start Date',selected_metric_df.index.min())
-    with col2:
+    with col6:
         endDate = st.date_input('End Date',selected_metric_df.index.max())
 
     if startDate < endDate:
@@ -97,28 +105,37 @@ def app():
     mask = (selected_metric_df.index > startDate) & (selected_metric_df.index <= endDate)
     selected_metric_df = selected_metric_df.loc[mask]
 
-    col3, col4 = st.columns((4,1))
-    with col3:
-        st.subheader('Metrics visualized')
-        explorer_chart = px.line(selected_metric_df)
-        explorer_chart.update_layout(autosize=True, showlegend=True ,legend_title_text='Metrics', margin=dict(l=20, r=30, t=10, b=20))
-        #explorer_chart.update_layout(dragmode = 'drawopenpath',newshape_line_color='cyan')
-        explorer_chart.update_layout(legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01))
-        explorer_chart.update_layout({'paper_bgcolor': 'rgba(0,0,0,0)', 'plot_bgcolor': 'rgba(0, 0, 0, 0)'})
-        explorer_chart.update_xaxes(title = 'Date',showline=True, linewidth=0.1, linecolor='#31333F', showgrid=False, gridwidth=0.1,mirror=True)
-        explorer_chart.update_yaxes(title = 'Metrics',showline=True, linewidth=0.1, linecolor='#31333F', showgrid=False, gridwidth=0.01,mirror=True)
-        st.plotly_chart(explorer_chart, use_container_width=True)
 
-    with col4:
-        st.info('''
-        - Selected metric: xx
-        - Selected metric : yy
-        - Selected metric: xx
-        - Selected metric : yy
-        ''')
+    st.subheader('Metrics visualized')
+    explorer_chart = px.line(selected_metric_df)
+    explorer_chart.update_layout(autosize=True, showlegend=True ,legend_title_text='Metrics', margin=dict(l=20, r=30, t=10, b=20))
+    explorer_chart.update_layout(legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01))
+    explorer_chart.update_layout({'paper_bgcolor': 'rgba(0,0,0,0)', 'plot_bgcolor': 'rgba(0, 0, 0, 0)'})
+    explorer_chart.update_xaxes(title = 'Date',showline=True, linewidth=0.1, linecolor='#31333F', showgrid=False, gridwidth=0.1,mirror=True)
+    explorer_chart.update_yaxes(title = 'Metrics',showline=True, linewidth=0.1, linecolor='#31333F', showgrid=False, gridwidth=0.01,mirror=True)
+    st.plotly_chart(explorer_chart, use_container_width=True)
 
-    st.subheader('Select a metric to see definition')
-    st.info('Write code to pass definitions here')
+
+    st.subheader('Metrics Definitions')
+    st.info('''
+    Write code to pass definitions here
+    - Total Value Deposited: Total Value Locked, is the dollar amount of all OHM staked in the protocol. 
+                            This metric is often used as growth or health indicator in DeFi projects.
+                            
+    - Treasury Market Value: Market Value of Treasury Assets, is the sum of the value (in dollars) of all assets held by the treasury.
+    
+    - Treasury Risk Free Value: Risk Free Value, is the amount of funds the treasury guarantees to use for backing OHM.
+    
+    - Reward Rate: Reward rate is the configured percentage of OHM distributed to all stakers on each rebase relative to the total supply. 
+                    The reward rate is precisely set by the policy team.
+    
+    - APY: Annual Percentage Yield, is the normalized representation of an interest rate,
+            based on a compounding period over one year. 
+            Note that APYs provided are rather ballpark level indicators and not so much precise future results.
+            
+    - Runway Available: Runway, is the number of days sOHM emissions can be sustained at a given rate. Lower APY = longer runway
+    
+    ''')
 
     st.subheader('Metrics in tabulated view')
     with st.expander("Click me!"):
