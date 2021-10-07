4f8ff221430bcb1d32808425783400433a926edd
Tachikoma000 <0thtachikoma@gmail.com>
2021-10-06 20:52:52 -0500

    explore page updates

--- Playgrounds_Application/Simulators/Experimental/mainDashboard.py
+++ Playgrounds_Application/Simulators/Experimental/mainDashboard.py
@@ -69,34 +69,53 @@ def app():
     st.title('Playgrounds Ω Explorer')
     st.markdown('''----''')
 
+    st.sidebar.info('Some information on what this page is about')
+    st.sidebar.info('Instructions')
+
     cols = ["ohmCirculatingSupply", "sOhmCirculatingSupply", "totalSupply"]
-    selected_metric = st.multiselect("Select a metric to explore", protocolMetrics_df.columns.tolist(),default=cols)
+    st.subheader('Select metrics to explore')
+    selected_metric = st.multiselect("",protocolMetrics_df.columns.tolist(),default=cols)
     selected_metric_df = protocolMetrics_df[selected_metric]
-    #selected_dates = st.date_input('Date Range', [datetime.date(selected_metric_df.index.min()),datetime.date(selected_metric_df.index.max())])
-    startDate = st.date_input('Start Date',selected_metric_df.index.min())
-    endDate = st.date_input('End Date',selected_metric_df.index.max())
+
+    st.subheader('Filter by date')
+    col1,col2 = st.columns((1,1))
+    with col1:
+        startDate = st.date_input('Start Date',selected_metric_df.index.min())
+    with col2:
+        endDate = st.date_input('End Date',selected_metric_df.index.max())
+
     if startDate < endDate:
         pass
     else:
         st.error('Error: Date out of possible range')
     mask = (selected_metric_df.index > startDate) & (selected_metric_df.index <= endDate)
     selected_metric_df = selected_metric_df.loc[mask]
-    #st.write(startDate)
-    #st.write(endDate)
-    #st.write(type(startDate))
-    #st.write(selected_metric_df.index.inferred_type == "datetime64")
 
+    col3, col4 = st.columns((4,1))
+    with col3:
+        st.subheader('Metrics visualized')
+        explorer_chart = px.line(selected_metric_df)
+        explorer_chart.update_layout(autosize=True, showlegend=True ,legend_title_text='Metrics', margin=dict(l=20, r=30, t=10, b=20))
+        explorer_chart.update_layout(dragmode = 'drawopenpath',newshape_line_color='cyan')
+        explorer_chart.update_layout(legend=dict(yanchor="top",y=0.99,xanchor="left",x=0.01))
+        explorer_chart.update_layout({'paper_bgcolor': 'rgba(0,0,0,0)', 'plot_bgcolor': 'rgba(0, 0, 0, 0)'})
+        explorer_chart.update_xaxes(title = 'Date',showline=True, linewidth=0.1, linecolor='#31333F', showgrid=False, gridwidth=0.1,mirror=True)
+        explorer_chart.update_yaxes(title = 'Metrics',showline=True, linewidth=0.1, linecolor='#31333F', showgrid=False, gridwidth=0.01,mirror=True)
+        st.plotly_chart(explorer_chart, use_container_width=True)
 
+    with col4:
+        st.info('''
+        - Selected metric: xx
+        - Selected metric : yy
+        - Selected metric: xx
+        - Selected metric : yy
+        ''')
 
-    explorer_chart = px.line(selected_metric_df)
-    explorer_chart.update_layout(autosize=True, showlegend=True ,legend_title_text='Metrics', margin=dict(l=20, r=30, t=10, b=20))
-    explorer_chart.update_layout(dragmode = 'drawopenpath',newshape_line_color='cyan')
-    explorer_chart.update_layout({'paper_bgcolor': 'rgba(0,0,0,0)', 'plot_bgcolor': 'rgba(0, 0, 0, 0)'})
-    explorer_chart.update_xaxes(title = 'Date',showline=True, linewidth=0.1, linecolor='#31333F', showgrid=False, gridwidth=0.1,mirror=True)
-    explorer_chart.update_yaxes(title = 'Metrics',showline=True, linewidth=0.1, linecolor='#31333F', showgrid=False, gridwidth=0.01,mirror=True)
-    st.plotly_chart(explorer_chart, use_container_width=True)
+    st.subheader('Select a metric to see definition')
+    st.info('Write code to pass definitions here')
 
-    with st.expander("See metrics table"):
+    st.subheader('Metrics in tabulated view')
+    with st.expander("Click me!"):
         st.write(selected_metric_df)
 
     st.write('---')
