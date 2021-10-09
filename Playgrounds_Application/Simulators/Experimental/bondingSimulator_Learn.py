702c0806b8a6ec54febd05af26234910712ec30f
Tachikoma000 <0thtachikoma@gmail.com>
2021-10-09 01:08:01 -0500

    Massive updates to ui/ux

--- Playgrounds_Application/Simulators/Experimental/bondingSimulator_Learn.py
+++ Playgrounds_Application/Simulators/Experimental/bondingSimulator_Learn.py
@@ -20,52 +20,65 @@ import base64
 # import awesome_streamlit as ast
 # endregion
 
-playgroundHome = Path(__file__).parents[1] / 'Assets/PG_bond.png'
+playgroundHome = Path(__file__).parents[1] / 'Assets/44_learn_2.png'
 playgroundHome  = Image.open(playgroundHome)
+
+learn_44 = Path(__file__).parents[1] / 'Assets/44_curve.png'
+learn_44  = Image.open(learn_44)
+
+
 def app():
 # region Description: All about staking
-
-    st.title('Learn: Bonding')
+    st.title('Learn: Bonding and (4,4)')
     st.markdown('''----''')
-    col1,col2 = st.columns((2.5,1.4))
+    col1,col2 = st.columns((1.5,1))
     with col1:
-        st.title('The Bonding strategy')
-        st.write(
-            '''
-            Bonding is the process of locking in a fixed reward in OHM. You trade in DAI for OHM at a discount
-            and the OHM is vested linearly over a period of 5 days
-    
-            As a bonder, you win if the price of OHM increases during your vesting period; when this happens you benefit from the
-            discount on OHM and the increase in price. You also win if price remains relatively the same during the vesting period.
-            This is becuase profits are still gained from the discount
-    
-            As a bonder, you loose if the price of OHM decreases during your vesting period. If this happens, you will have to 
-            decide between OHM and SLP (whichever is worth more)
-    
-            **So what is (4,4) and what does it have to do with Bonding?** 
-            The (4,4) strategy is a maximizing strategy that combines the benefits of staking (3,3) and bonding (1,1). 
-            (4,4) simply means staking available OHMS during the vesting period to capture staking rewards during the vesting period
-
-            ''')
-    st.markdown('''----''')
-    st.title('What is (4,4) Playground? ')
-    col3,col4 = st.columns((0.5,0.3))
-    with col3:
-        st.write(
-            """
-            **(4,4) Playground is a simulator for understanding and forecasting the bond, claim, and stake strategy**
-
-            Use this simulator to:
-            - Strategize (4,4) profitability 
-            - Forecast additional gains from using (4,4) compared to (3,3)
-            - Forecast additional gains from staking bonding emissions at varying epochs
-            - OHM growth over time with (4,4) strategy
-            """
-        )
-    with col4:
+        st.title('')
         st.image(playgroundHome)
+    with col2:
+        st.title('The Bonding strategy')
+        bondLearnExpander = st.expander(label = 'Click to view Bonding Strategy', expanded=True)
+        with bondLearnExpander:
+            st.write(
+                '''
+                Bonding is the process of locking in a fixed reward in OHM. You trade in DAI for OHM at a discount
+                and the OHM is vested linearly over a period of 5 days
+                
+                As a bonder, you win if the price of OHM increases during your vesting period; when this happens you benefit from the
+                discount on OHM and the increase in price. You also win if price remains relatively the same during the vesting period.
+                This is becuase profits are still gained from the discount
+    
+                As a bonder, you loose if the price of OHM decreases during your vesting period. If this happens, you will have to 
+                decide between OHM and SLP (whichever is worth more)
+                ''')
+        st.title('The (4,4) strategy')
+        fourFourLearnEpander = st.expander(label = 'click to view (4,4) Strategy', expanded=False)
+        with fourFourLearnEpander:
+            st.write(
+                """
+                The (4,4) strategy is a maximizing strategy that combines the benefits of staking (3,3) and bonding (1,1). 
+                (4,4) simply means staking available OHMS during the vesting period to capture staking rewards during the vesting period
+                """)
+    st.markdown('''----''')
 
+    st.title('What is (4,4) Playground? ')
+    col3,col4 = st.columns((1.5,1))
+    with col3:
+        fourFourLearnEpander_2 = st.expander(label = 'click to view (4,4) Strategy', expanded=True)
+        with fourFourLearnEpander_2:
+                st.write(
+                    """
+                    **(4,4) Playground is a simulator for understanding and forecasting the bond, claim, and stake strategy**
 
+                    Use this simulator to:
+                    - Strategize (4,4) profitability 
+                    - Forecast additional gains from using (4,4) compared to (3,3)
+                    - Forecast additional gains from staking bonding emissions at varying epochs
+                    - OHM growth over time with (4,4) strategy
+                    """
+                )
+    with col4:
+        st.image(learn_44)
     st.markdown('''----''')
     st.info('''
     Learn more here: https://docs.olympusdao.finance/protocol-internals/market-dynamics
--- Playgrounds_Application/Simulators/Experimental/stakingSimulator_Learn.py
+++ Playgrounds_Application/Simulators/Experimental/stakingSimulator_Learn.py
@@ -11,55 +11,76 @@ import pathlib  # url management
 import plotly.express as px  # cleaner graphs
 import plotly.graph_objects as go  # cleaner graphs
 import streamlit as st
+from PIL import Image
+from pathlib import Path
 # import awesome_stream lit as ast
 # endregion
+
+learnStaking_logo = Path(__file__).parents[1] / 'Assets/LEARN_STAKING_2.png'
+learnStaking_logo  = Image.open(learnStaking_logo)
+
+learnStaking = Path(__file__).parents[1] / 'Assets/three.png'
+learnStaking  = Image.open(learnStaking)
+
+learnStaking_33 = Path(__file__).parents[1] / 'Assets/33_curve.png'
+learnStaking_33  = Image.open(learnStaking_33)
+
+
 def app():
 # region Description: All about staking
 
-    st.title('Learn: Staking')
+    col1,col2 = st.columns((0.6,1))
+    with col1:
+        st.image(learnStaking_logo)
     st.markdown('''----''')
     st.title('The Staking Strategy')
-    col1,col2 = st.columns((2.5,1.4))
-    with col1:
-        st.write(
-            '''
-            **Staking is the primary value accrual strategy of Olympus**. Stakers stake their OHM on the Olympus website to earn rebase rewards. 
-            The rebase rewards come from the proceed from bond sales, and can vary based on the number of OHM staked in the protocol and the reward rate set by monetary policy
-        
-            **Staking is a passive, long-term strategy**. The increase in your stake of OHM translates into a constantly falling cost basis converging on zero. 
-            This means even if the market price of OHM drops below your initial purchase price, given a long enough staking period, the increase in your staked OHM balance should eventually outpace the fall in price
-        
-            **When you stake, you lock OHM and receive an equal amount of sOHM**. Your sOHM balance rebases up automatically at the end of every epoch. 
-            sOHM is transferable and therefore composable with other DeFi protocols
-        
-            **When you unstake, you burn sOHM and receive an equal amount of OHM**. Unstaking means the user will forfeit the upcoming rebase reward. 
-            Note that the forfeited reward is only applicable to the unstaked amount; the remaining staked OHM (if any) will continue to receive rebase rewards
-            
-            ''')
-    st.markdown('''----''')
-    st.title('What is (3,3) Playground? ')
-    col3,col4 = st.columns((0.5,1))
+    col3,col4 = st.columns((2.5,1.4))
     with col3:
-        st.write(
-            """
-            **(3,3) Playground is a simulator for staking, and income strategies**
+        stakeLearnExpander = st.expander(label='Click to view Staking Strategy', expanded=True)
+        with stakeLearnExpander:
+            st.write(
+                '''
+                **Staking is the primary value accrual strategy of Olympus**. Stakers stake their OHM on the Olympus website to earn rebase rewards. 
+                The rebase rewards come from the proceed from bond sales, and can vary based on the number of OHM staked in the protocol and the reward rate set by monetary policy
+        
+                **Staking is a passive, long-term strategy**. The increase in your stake of OHM translates into a constantly falling cost basis converging on zero. 
+                This means even if the market price of OHM drops below your initial purchase price, given a long enough staking period, the increase in your staked OHM balance should eventually outpace the fall in price
+        
+                **When you stake, you lock OHM and receive an equal amount of sOHM**. Your sOHM balance rebases up automatically at the end of every epoch. 
+                sOHM is transferable and therefore composable with other DeFi protocols
+        
+                **When you unstake, you burn sOHM and receive an equal amount of OHM**. Unstaking means the user will forfeit the upcoming rebase reward. 
+                Note that the forfeited reward is only applicable to the unstaked amount; the remaining staked OHM (if any) will continue to receive rebase rewards
             
-            Use this simulator to:
-            - Forecast ROI ar current and future reward yield percent
-            - OHM growth over time
-            - OHM and USD value over time
-            """)
+                ''')
+    st.markdown('''----''')
     with col4:
-        st.write(
-        """    
-        We love the income, use this simulator to strategize:
-        - Required staked OHM to reach desired daily income
-        - Count down until you are earning desired daily income
-        - Required staked OHM to reach desired weekly income
-        - Count down until you are earning desired weekly income
-        """)
+        st.image(learnStaking)
+    st.title('What is (3,3) Playground? ')
+    col5,col6 = st.columns((1,1))
+    with col5:
+        st.image(learnStaking_33)
+    with col6:
+        threeThreeLearnExpander = st.expander(label='Click to view (3,3) Strategy', expanded=True)
+        with threeThreeLearnExpander:
+            st.write(
+                """
+                **(3,3) Playground is a simulator for staking, and income strategies**
 
+                Use this simulator to:
+                - Forecast ROI ar current and future reward yield percent
+                - OHM growth over time
+                - OHM and USD value over time  
+                """)
 
+            st.write(
+                """    
+                Also, use this simulator to strategize:
+                - Required staked OHM to reach desired daily income
+                - Count down until you are earning desired daily income
+                - Required staked OHM to reach desired weekly income
+                - Count down until you are earning desired weekly income
+                """)
     st.markdown('''----''')
 
 
